21ef0f4c6f6a290f6aa7e799babee3aa65bbb356
Richard Braamburg <richard.braamburg@gmail.com>
2022-08-29 10:07:22 -0400

    small updates

--- dev_update_tx_regions.py
+++ dev_update_tx_regions.py
@@ -4,18 +4,21 @@ import json
 
 
 mapping = {'Region 1': 69, 'Region 2': 70, 'Region 3': 71}
-access_key = {"authorization": "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZGYzMzYxZWIzZWMyZjM3YzhmNDEwMjA1MzRjMTc5YWJhNWVhZmNmZmRhMDI0NWEyMmI2MDEzMWFlZjMwYzQ4Iiwic3ViIjoiOSIsImV4cCI6MTY2MTU5MDgwMCwiaXNzIjoiSGVhZHdheSIsInNjb3BlcyI6WyJkZWZhdWx0Il0sImlhdCI6MTY2MTUzMjkzOSwicm9sZXMiOlsiQVRMQVNfVVNFUiIsIlpPQ0RPQyIsIkFETUlOIiwiUFJPVklERVJfSU1QRVJTT05BVE9SIiwiUEFUSUVOVF9JTVBFUlNPTkFUT1IiLCJJTlRFUk5BTF9XRUJIT09LIiwiUEhJX1ZJRVdFUiJdfQ.X8PZFYxJGs8YwgyNnDi3NPkPzJ9QESMTL_8B7DdcvaY"}
+access_key = {"authorization": "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MzdiNmNlZTBiYmIyMzM5NDViNDhmMzQzZjU3OTE1YmY5ZDRiZjdkY2NkZDgwMTc2ZmRlOTQxN2NkMWE4NGQyIiwic3ViIjoiOSIsImV4cCI6MTY2MTg1MDAwMCwiaXNzIjoiSGVhZHdheSIsInNjb3BlcyI6WyJkZWZhdWx0Il0sImlhdCI6MTY2MTc4MTE1MCwicm9sZXMiOlsiQVRMQVNfVVNFUiIsIlpPQ0RPQyIsIkFETUlOIiwiUFJPVklERVJfSU1QRVJTT05BVE9SIiwiUEFUSUVOVF9JTVBFUlNPTkFUT1IiLCJJTlRFUk5BTF9XRUJIT09LIiwiUEhJX1ZJRVdFUiJdfQ.emYwK0mEFFL7hnQV_uMuvpfDy7HlKs_z7210QwETYRQ"}
 dev_url = "http://localhost:5000/provider/"
 prod_url = "https://headway.co/provider/"
+test_file = "dev_source.csv"
+prod_file = "BCSBTX Region Mapping.csv"
 
 
 def main():
-    with open('dev_source.csv', 'r', encoding='utf-8-sig') as source:
+    with open(test_file, 'r', encoding='utf-8-sig') as source:
         provider_data = csv.DictReader(source)
         for row in provider_data:
             provider = row["Providers Provider ID"]
             region = row["Final Region"]
             if validate_source(provider, region):
+                print(provider, region, mapping[region])
                 update_region(provider, region)
 
 
@@ -37,9 +40,9 @@ def update_region(provider_id, region):
         res = requests.put(f"{dev_url}{provider_id}",
                            headers=access_key,
                            data=json_body)
-        if res.status_code != 200:
+        if res.status_code != 200 and res.status_code != 210:
             print(
-                f"{res.status_code} Failed to update provider's {provider_id} region")
+                f"{res.status_code} Failed to update provider's {provider_id} region]")
             # print(res.json())
         else:
             print(f"Successfully updated provider's {provider_id} region")
