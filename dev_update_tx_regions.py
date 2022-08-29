984eb9ff52da5e145a1c0af412214f93dc01290c
Richard Braamburg <richard.braamburg@gmail.com>
2022-08-29 13:32:12 -0400

    Final Version

--- dev_update_tx_regions.py
+++ dev_update_tx_regions.py
@@ -4,22 +4,29 @@ import json
 
 
 mapping = {'Region 1': 69, 'Region 2': 70, 'Region 3': 71}
-access_key = {"authorization": "bearer xxx"}
+access_key = {"authorization": "bearer abc123"}
 dev_url = "http://localhost:5000/provider/"
-prod_url = "https://headway.co/provider/"
+prod_url = "https://api.headway.co/provider/"
 test_file = "dev_source.csv"
 prod_file = "BCSBTX Region Mapping.csv"
 
 
 def main():
-    with open(test_file, 'r', encoding='utf-8-sig') as source:
+    with open(prod_file, 'r', encoding='utf-8-sig') as source:
         provider_data = csv.DictReader(source)
+        row_count = 0
         for row in provider_data:
             provider = row["Providers Provider ID"]
             region = row["Final Region"]
             if validate_source(provider, region):
                 print(provider, region, mapping[region])
                 update_region(provider, region)
+            else:
+                print(f"{provider} - {region} - validation failed")
+            row_count += 1
+            print(f"Cycle: {row_count}")
+            # if row_count >= 1050:
+            #     break
 
 
 def validate_source(provider, region):
@@ -35,20 +42,21 @@ def update_region(provider_id, region):
     new_region = {"providerLicenseState": {
         "providerRegionId": mapped_region}}
     json_body = json.dumps(new_region)
-
+    print(f"{prod_url}{provider_id}")
     try:
-        res = requests.put(f"{dev_url}{provider_id}",
+        res = requests.put(f"{prod_url}{provider_id}",
                            headers=access_key,
                            data=json_body)
         if res.status_code != 200 and res.status_code != 210:
             print(
-                f"{res.status_code} Failed to update provider's {provider_id} region]")
+                f"{res.status_code} - {provider_id} - {region} - Failed to update provider's {provider_id} region")
             # print(res.json())
         else:
-            print(f"Successfully updated provider's {provider_id} region")
+            print(
+                f"{res.status_code} - {provider_id} - {region} - Successfully updated provider's {provider_id} region")
     except Exception as e:
         print(
-            f"Exception: Failed to update provider's {provider_id} region.\nError: {str(e)}")
+            f"{res.status_code} - {provider_id} - {region} - Exception: Failed to update provider's {provider_id} region.\nError: {str(e)}")
 
 
 if __name__ == "__main__":
